e53bcb051a441d5b7d46d71ccdaa4510fc257da0
Priya2123 <aggarwalpriya1121@gmail.com>
2021-03-04 13:59:40 +0530

    Responsiveness WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from "react";
-import { Grid, Typography, Button } from "@material-ui/core";
+import { Grid, Typography, Container } from "@material-ui/core";
 import { useStyles } from "./AppStyles.js";
-import scarecrow from './assets/Scarecrow.png';
+import scarecrow from "./assets/Scarecrow.png";
 
 const App = () => {
   const classes = useStyles();
   return (
     <>
-      <Grid container lg={12} md={12} style={{ marginTop: "1rem" }}>
-        <Grid item lg={12} md={12}>
-          <Typography className={classes.nav}>404 NOT FOUND</Typography>
+      <Container fixed>
+        <Grid container lg={12} md={12} style={{ marginTop: "1rem" }}>
+          <Grid item lg={12} md={12}>
+            <Typography className={classes.nav}>404 NOT FOUND</Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container lg={12} md={12} style={{marginTop: '5rem'}}>
-        <Grid item lg={6} md={6}>
-          <img src={scarecrow} className={classes.image} alt="scarecrow"/>
+        <Grid
+          container
+          lg={12}
+          md={12}
+          xs={12}
+          style={{ marginTop: "5rem" }}
+          justify="center"
+        >
+          <Grid item lg={6} md={6} xs={12}>
+            <img src={scarecrow} className={classes.image} alt="scarecrow" />
+          </Grid>
+          <Grid item lg={5} md={5} xs={11}>
+            <Typography className={classes.headone}>
+              I have bad news for you
+            </Typography>
+            <Typography className={classes.body}>
+              The page you are looking for might be removed or is temporarily
+              unavailable
+            </Typography>
+            <button className={classes.btn}>
+              <Typography className={classes.btntext}>
+                BACK TO HOMEPAGE
+              </Typography>
+            </button>
+          </Grid>
         </Grid>
-        <Grid item lg={6} md={6}>
-          <Typography className={classes.headone}>I have bad news for you</Typography>
-          <Typography className={classes.body}>The page you are looking for might be removed or is temporarily unavailable</Typography>
-          <Button>BACK TO HOMEPAGE</Button>
+      </Container>
+      <Grid
+        container
+        lg={12}
+        md={12}
+        xs={12}
+        sm={12}
+        className={classes.footerrow}
+      >
+        <Grid item lg={12} md={12} xs={12} sm={12}>
+          <Typography className={classes.footer} variant="caption">
+            created by{" "}
+            <a
+              className={classes.link}
+              style={{ fontWeight: "bold" }}
+              href="https://github.com/Priya2123"
+            >
+              Priya Aggarwal
+            </a>{" "}
+            -{" "}
+            <a className={classes.link} href="https://devchallenges.io/">
+              devchallenges.io
+            </a>
+          </Typography>
         </Grid>
       </Grid>
     </>
--- src/AppStyles.js
+++ src/AppStyles.js
@@ -9,25 +9,84 @@ export const useStyles = makeStyles((theme) => ({
     letterSpacing: "-0.08em",
     color: "#333333",
     marginLeft: "3rem",
+    [theme.breakpoints.down("xs")]: {
+        fontSize: "22px",
+        marginLeft: '0'
+      },
   },
   image: {
-    height: "390px",
-    width: "520px",
-    marginLeft: "3rem",
+    height: "350px",
+    width: "480px",
+    [theme.breakpoints.down("xs")]: {
+        height: "200px",
+        width: "290px",
+        marginLeft: '0',
+        marginBottom: '3rem'
+      },
   },
   headone: {
     fontFamily: "Space Mono",
     fontWeight: "bold",
-    fontSize: "58px",
+    fontSize: "52px",
     fontStyle: "normal",
     letterSpacing: "-0.035em",
+    color: "#333333",
+    [theme.breakpoints.down("xs")]: {
+       fontSize:'40px'
+      },
   },
   body: {
     fontFamily: "Space Mono",
-    fontSize: "22px",
+    fontSize: "20px",
     fontWeight: "normal",
     letterSpacing: "-0.035em",
-    width:'22rem',
-    marginTop: '1rem'
+    width: "22rem",
+    marginTop: "1rem",
+    color: "#4F4F4F",
+    [theme.breakpoints.down("xs")]: {
+        fontSize:'17px',
+        width:'100%'
+       },
+  },
+  btn: {
+    backgroundColor: "#333333",
+    color: "#fff",
+    padding: "0.8rem 2rem",
+    marginTop: "2.5rem",
+    textAlign: "center",
+    [theme.breakpoints.down("xs")]: {
+        padding: '0.7rem 1.7rem',
+       },
+  },
+  btntext: {
+    color: "#FFFFFF",
+    fontFamily: "Space Mono",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    letterSpacing: "-0.035em",
+    fontSize: "12px",
+  },
+  footerrow: {
+    color: "#BDBDBD",
+    textAlign: "center",
+    // position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    [theme.breakpoints.down("xs")]: {
+      padding: "1rem 0",
+    },
+  },
+  footer: {
+    fontFamily: "Montserrat",
+    fontWeight: "500",
+    fontSize: "14px",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "10px",
+    },
+  },
+  link: {
+    color: "grey",
+    textDecoration: "none",
   },
 }));
